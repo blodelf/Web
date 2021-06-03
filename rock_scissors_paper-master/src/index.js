58f170ffe90c5d520597d486e5859d6ae6f28457
Aleksandr Lukashov <56129898+blodelf@users.noreply.github.com>
2021-06-04 00:12:10 +0300

    Delete rock_scissors_paper-master directory

--- rock_scissors_paper-master/src/index.js
+++
@@ -1,9 +0,0 @@
-import Nav from "./scripts/nav.js";
-import "./styles/site.css";
-import * as header from "./scripts/header.js"
-import * as footer from "./scripts/footer.js"
-
-
-header.fillHeader();
-footer.fillFooter();
-new Nav();
